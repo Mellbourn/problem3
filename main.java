65f396eb8af2510ccd6dca65a059ad7e04e7f20d
Klas Mellbourn <klas@mellbourn.net>
2021-09-10 12:15:02 +0200

    implement problem

--- main.java
+++ main.java
@@ -1,7 +1,15 @@
-// Your First Program
+class Random {
 
-class HelloWorld {
+    private static final int randomDigit() {
+        return (int) (Math.random() * 10);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        for (var row = 0; row < 5; row++) {
+            for (var digit = 0; digit < 6; digit++) {
+                System.out.print(randomDigit());
+            }
+            System.out.println();
+        }
     }
 }
